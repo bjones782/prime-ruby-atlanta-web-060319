e2217390fdc3bafa71f211b351e6feedabf57aca
Brooke Jones <bjones782@gmail.com>
2019-05-13 19:44:51 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -21,5 +21,21 @@
     next unless p
     break if p*p > max
     counter += 1
+  # Start at the square of the current number, and step through.
+  # Go up to the max value, by multiples of the current number, and replace
+  # that value with nil in the primes array
+    (p*p).step(max,p) { |m| primes[m] = nil }
+  end
+
+  # Finally, return the compacted array.
+    puts "Solved for #{max} in #{counter} steps."
+    primes.compact
+end
+
+def prime?(num)
+  sieve(num).include?(num)
+end
+
+puts prime?(105557)
   
     
