9e1acdc50cb8d2cc06d82dd10ee8b42c067cd3e0
Brooke Jones <bjones782@gmail.com>
2019-05-13 19:20:57 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,11 +3,13 @@
  def prime?(integer)
     #Returns True if n is prime
     if n <= 1
-    if n == 2:
+        return False 
+    if n == 2
         return True
-    if n == 3:
+    if n == 3
         return True
-    if n % 2 == 0:
+    if n % 2 == 0
         return False
-    if n % 3 == 0:
+    if n % 3 == 0
         return False
+    
